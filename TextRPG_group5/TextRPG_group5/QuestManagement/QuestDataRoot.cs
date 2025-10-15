ad987f0fb7a3c6e46636fd0a086a5945fdac2523
OhMinGeun9609 <oksk199428@gmail.com>
2025-10-15 12:18:37 +0900

    feat : 퀘스트 데이터 관련 클래스 추가
    - QuestInfo.json 파일을 읽어올 클래스 LoadQuest클래스 추가
    - 불러온 내용을 묶어서 리스트에 저장할 QuestDataRoot클래스 추가

---
+++ TextRPG_group5/TextRPG_group5/QuestManagement/QuestDataRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_group5.QuestManagement
+{
+    public class QuestDataRoot
+    {
+        public List<QuestManager> quests { get; set; }
+
+    }
+}
