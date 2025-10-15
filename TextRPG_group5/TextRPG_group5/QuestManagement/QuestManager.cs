5af6c600e47e2f568f3fb0881daf0f8e3d543463
OhMinGeun9609 <oksk199428@gmail.com>
2025-10-15 18:47:09 +0900

    refactor, feat : 퀘스트시스템 리팩토링, 기능추가
    - 퀘스트 진행 상태 변경 메서드 추가
    - 보상수령 메서드 추가
    - 기존 LoadQuest메서드의 기능을
    새 QuestManager 클래스로 이관
    - 기존 QuestManager메서드의 이름을  Quest로 변경

--- TextRPG_group5/TextRPG_group5/QuestManagement/QuestManager.cs
+++ TextRPG_group5/TextRPG_group5/QuestManagement/QuestManager.cs
@@ -3,23 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Text.Json;
 
 namespace TextRPG_group5.QuestManagement
 {
-    public enum QuestStatus
+    public class QuestManager : Exception
     {
-        NoProgress,
-        InProgress,
-        Complete
-    }
-    public class QuestManager
-    {
-        public int QuestID { get; set; }
-        public string QuestTitle { get; set; }
-        public string QuestDescription { get; set; }
-        public List<Required> objectives { get; set; }
-        public Rewards Rewards { get; set; }
-        public QuestStatus Status { get; set; }
+        public List<Quest> Quests { get; private set; }
+
+        public void Load(string path)
+        {
+            try
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true, // 대소문자 무시
+                    ReadCommentHandling = JsonCommentHandling.Skip, // 주석 허용
+                    AllowTrailingCommas = true // 마지막 쉼표 허용
+                };
+
+                string json = File.ReadAllText(path, Encoding.UTF8);
+                Quests = JsonSerializer.Deserialize<List<Quest>>(json, options);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FileLoadException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void AcceptQuest(int id)
+        {
+            Quest q = Quests.FirstOrDefault(x => x.QuestID == id);
+            if(q != null && q.Status == QuestStatus.NoProgress)
+            {
+                q.Status = QuestStatus.InProgress;
+                Console.WriteLine($"[퀘스트를 수락하셨습니다.] {q.QuestTitle}");
+            }
+        }
+
+        public void UpdateProgress(string target)
+        {
+            foreach(var q in Quests.Where(x => x.Status == QuestStatus.InProgress))
+            {
+                foreach(var o in q.objectives)
+                {
+                    o.current++;
+                    Console.WriteLine($"[진행] {o.Target} : {o.current}/{o.Count}");
+
+                    if(q.CheckCompletion())
+                    {
+                        q.Status = QuestStatus.Complete;
+                        Console.WriteLine($"[완료] {q.QuestTitle}");
+                    }
+                }
+            }
+        }
+
+        public void GiveReward()// Todo : 매개변수에 Player오브젝트 추가
+        {
+            foreach(var q in Quests.Where(x => x.Status == QuestStatus.Complete))
+            {
+                // Todo : 플레이어의 경험치, 돈, 인벤토리 조작 로직 추가
+            }
+        }
+
+
     }
 }
