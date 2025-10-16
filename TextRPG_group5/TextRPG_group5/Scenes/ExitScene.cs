3c086f2c80e164fcb0994c826b7600c3152ed46d
Kimtaehwan <coseltm123@gmail.com>
2025-10-16 14:22:24 +0900

    ExitScene

---
+++ TextRPG_group5/TextRPG_group5/Scenes/ExitScene.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using TextRPG_group5.Scenes;
+
+
+namespace TextRPG_group5.Scenes
+{
+    internal class ExitScene : Exit
+
+    {
+        private static ExitScene scene;
+        private static GameProgress gameProgress;
+
+        public bool ShouldExitGame { get; private set; } = false;
+        public static ExitScene GetInstance(GameProgress gp)
+        {
+            if (scene == null)
+            {
+                scene = new ExitScene();
+            }
+            gameProgress = gp;
+            return scene;
+        }
+    }
+}
