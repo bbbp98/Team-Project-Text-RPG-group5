7dd96e3ab490cc52db9376e555401f6a234fb878
zlkj483 <mlimsg08@gmail.com>
2025-10-16 11:00:07 +0900

    docs: 문서 수정

--- TextRPG_group5/TextRPG_group5/GamePlayer.cs
+++ TextRPG_group5/TextRPG_group5/GamePlayer.cs
@@ -6,113 +6,113 @@ using System.Threading.Tasks;
 using TextRPG_group5.ItemManagement;
 namespace TextRPG_group5
 {
-        
-        public class Player : Character
-        {
-            public string Job { get; private set; }
-            public int MaxMp { get; private set; }
-            public int NowMp { get; private set; }
-            public int Gold { get; set; }
-            public int Exp { get; private set; }
-            public int MaxExp { get; private set; }
-            public int ReachedStage { get; set; }
+
+    public class Player : Character
+    {
+        public string Job { get; private set; }
+        public int MaxMp { get; private set; }
+        public int NowMp { get; private set; }
+        public int Gold { get; set; }
+        public int Exp { get; private set; }
+        public int MaxExp { get; private set; }
+        public int ReachedStage { get; set; }
 
 
         public Player(string name, string job)
                 : base(name, 0, 0, 0)
-            {
+        {
             Level = 1;
             Critical = 0.1;
             Evasion = 0.2;
-                Gold = 500;
-                Exp = 0;
-                MaxExp = 100;
+            Gold = 500;
+            Exp = 0;
+            MaxExp = 100;
             Equipment = new PlayerItem(this);
             while (true)
+            {
+                if (StatbyJob(job))
                 {
-                    if (StatbyJob(job))
-                    {
-                        Job = job;
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("전사, 궁수, 도적, 법사 중 하나를 선택하시오");
-                        job = Console.ReadLine();
-                    }
+                    Job = job;
+                    break;
                 }
-                NowHp = MaxHp;
-                NowMp = MaxMp;
+                else
+                {
+                    Console.WriteLine("전사, 궁수, 도적, 법사 중 하나를 선택하시오");
+                    job = Console.ReadLine();
+                }
             }
-        public Player() 
+            NowHp = MaxHp;
+            NowMp = MaxMp;
+        }
+        public Player()
         {
             Equipment = new PlayerItem(this);
         }
 
-            private bool StatbyJob(string job)
+        private bool StatbyJob(string job)
+        {
+            switch (job)
             {
-                switch (job)
-                {
-                    case "전사":
-                        Attack = 30; Defence = 50; MaxHp = 120; MaxMp = 60; Critical = 0.1; Evasion = 0.1;
-                        return true;
-                    case "궁수":
-                        Attack = 40; Defence = 30; MaxHp = 90; MaxMp = 80; Critical = 0.4; Evasion = 0.2;
-                        return true;
-                    case "도적":
-                        Attack = 50; Defence = 20; MaxHp = 80; MaxMp = 90; Critical = 0.3; Evasion = 0.3;
-                        return true;
-                    case "법사":
-                        Attack = 20; Defence = 20; MaxHp = 60; MaxMp = 120; Critical = 0.2; Evasion = 0.1;
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-            public override void ShowStatus()
-            {
-                Console.WriteLine("===========Player status===========");
-                Console.WriteLine($"이름: {Name}");
-                Console.WriteLine($"직업: {Job}");
-                Console.WriteLine($"레벨: {Level}");
-                Console.WriteLine($"경험치: {Exp}/{MaxExp}");
-                Console.WriteLine($"체력: {NowHp}/{MaxHp}");
-                Console.WriteLine($"마나: {NowMp}/{MaxMp}");
-                Console.WriteLine($"공격력: {Attack}");
-                Console.WriteLine($"방어력: {Defence}");
-                Console.WriteLine($"치명타 확률: {Critical * 100}");
-                Console.WriteLine($"회피확률: {Evasion * 100}");
-                Console.WriteLine($"소지금: {Gold}");
-                Console.WriteLine($"도달 스테이지: [{ReachedStage}]Stage");
-                Console.WriteLine("===================================\n");
-            }
-            public void GainExp(int amount)
-            {
-                Exp += amount;
-                Console.WriteLine($"경험치 {amount} 획득.");
-                while(Exp >= MaxExp)
-                {
-                    Exp -= MaxExp;
-                    LevelUp();
-                }
-            }
-            public void LevelUp()
-            {
-                Level++;
-                MaxExp = (int)(MaxExp * 1.2);
-                Attack += 2; Defence += 2; MaxHp += 10; MaxMp += 5;
-                NowHp = MaxHp; NowMp = MaxMp;
-                Console.WriteLine($"레벨 업 하였습니다.\n 현재 레벨 : {Level} 입니다.");
-            }
-            public PlayerItem Equipment { get; private set; }
-            public void AddCritical(double amount)
-            {
-                Critical += amount;
-            }
-            public void AddEvasion(double amount)
-            {
-                Evasion += amount;
+                case "전사":
+                    MaxHp = 120; Attack = 30; Defence = 50; Critical = 0.1; Evasion = 0.1; MaxMp = 60;
+                    return true;
+                case "궁수":
+                    MaxHp = 90; Attack = 40; Defence = 30; Critical = 0.4; Evasion = 0.2; MaxMp = 80;
+                    return true;
+                case "도적":
+                    MaxHp = 80; Attack = 50; Defence = 20; Critical = 0.3; Evasion = 0.3; MaxMp = 90;
+                    return true;
+                case "법사":
+                    MaxHp = 60; Attack = 20; Defence = 20; Critical = 0.2; Evasion = 0.1; MaxMp = 120;
+                    return true;
+                default:
+                    return false;
             }
         }
+        public override void ShowStatus()
+        {
+            Console.WriteLine("===========Player status===========");
+            Console.WriteLine($"이름: {Name}");
+            Console.WriteLine($"직업: {Job}");
+            Console.WriteLine($"레벨: {Level}");
+            Console.WriteLine($"경험치: {Exp}/{MaxExp}");
+            Console.WriteLine($"체력: {NowHp}/{MaxHp}");
+            Console.WriteLine($"마나: {NowMp}/{MaxMp}");
+            Console.WriteLine($"공격력: {Attack}");
+            Console.WriteLine($"방어력: {Defence}");
+            Console.WriteLine($"치명타 확률: {Critical * 100}");
+            Console.WriteLine($"회피확률: {Evasion * 100}");
+            Console.WriteLine($"소지금: {Gold}");
+            Console.WriteLine($"도달 스테이지: [{ReachedStage}]Stage");
+            Console.WriteLine("===================================\n");
+        }
+        public void GainExp(int amount)
+        {
+            Exp += amount;
+            Console.WriteLine($"경험치 {amount} 획득.");
+            while (Exp >= MaxExp)
+            {
+                Exp -= MaxExp;
+                LevelUp();
+            }
+        }
+        public void LevelUp()
+        {
+            Level++;
+            MaxExp = (int)(MaxExp * 1.2);
+            Attack += 2; Defence += 2; MaxHp += 10; MaxMp += 5;
+            NowHp = MaxHp; NowMp = MaxMp;
+            Console.WriteLine($"레벨 업 하였습니다.\n 현재 레벨 : {Level} 입니다.");
+        }
+        public PlayerItem Equipment { get; private set; }
+        public void AddCritical(double amount)
+        {
+            Critical += amount;
+        }
+        public void AddEvasion(double amount)
+        {
+            Evasion += amount;
+        }
     }
+}
 
