992e877138c9ba6ac6529db807638d01173d69c3
bbbp98 <qjarms1300@gmail.com>
2025-10-17 11:29:22 +0900

    feat: 타이틀, 캐릭터 생성 씬 연동

--- TextRPG_group5/TextRPG_group5/Program.cs
+++ TextRPG_group5/TextRPG_group5/Program.cs
@@ -1,4 +1,5 @@
-﻿using TextRPG_group5.ItemManage;
+﻿using System.Runtime.CompilerServices;
+using TextRPG_group5.ItemManage;
 using TextRPG_group5.Managers;
 using TextRPG_group5.Scenes;
 
@@ -6,33 +7,37 @@ namespace TextRPG_group5
 {
      internal class Program
      {
-          static private Scene? currentScene;
+          static private Scene? currentScene = new NewTitleScene();
           static private Player? player;
 
           public const int maxStage = 10;
-          //static private GameProgress gameProgress = new GameProgress();
 
           static void Main(string[] args)
           {
-               Initialize();
+               //Initialize();
 
                while (true)
                {
-                    currentScene!.Show();
-                    Console.WriteLine();
-                    Console.WriteLine("원하시는 행동을 입력해주세요.");
-                    Console.Write(">> ");
-                    byte input = byte.TryParse(Console.ReadLine(), out byte val) ? val : byte.MaxValue;
-                    Console.Clear();
-                    currentScene.HandleInput(input);
+                    if (currentScene is NewTitleScene
+                         || currentScene is CreatePlayerScene)
+                    { currentScene!.Show(); }
+                    else
+                    {
+                         currentScene!.Show();
+                         Console.WriteLine();
+                         Console.WriteLine("원하시는 행동을 입력해주세요.");
+                         Console.Write(">> ");
+                         byte input = byte.TryParse(Console.ReadLine(), out byte val) ? val : byte.MaxValue;
+                         Console.Clear();
+                         currentScene.HandleInput(input);
+                    }
                }
           }
 
-          static private void Initialize()
+          static public void Initialize(Player newPlayer)
           {
                // player initialize
-               // load player data OR create player data 
-               player = new Player("group5", "전사"); // test player
+               player = newPlayer;
                player.ReachedStage = 1;
 
                // test items initialize
@@ -43,7 +48,6 @@ namespace TextRPG_group5
                player.Inventory.AddItem(ItemInfo.GetItem("가죽갑옷"));
                player.Inventory.AddItem(ItemInfo.GetItem("숙련자의 로브"));
                player.Inventory.AddItem(ItemInfo.GetItem("MP소형포션"));
-               //gameProgress = new GameProgress();
 
                // scene initialize
                currentScene = new MainScene(player); // startScene
---
+++ TextRPG_group5/TextRPG_group5/Scenes/CreatePlayerScene.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextRPG_group5.ItemManage;
+
+namespace TextRPG_group5.Scenes
+{
+     internal class CreatePlayerScene : Scene
+     {
+          enum CheckType
+          {
+               Name,
+               Job,
+          }
+
+          Player? player;
+          string name = "";
+          string job = "";
+
+          public override void HandleInput(byte input)
+          {
+          }
+
+          public override void Show()
+          {
+               Console.Clear();
+
+               Naming();
+               SelectClass();
+
+               player = new Player(name, job);
+
+               Console.Clear();
+               Console.WriteLine($"{player.Name}, {player.Job}(이)가 생성되었습니다!");
+               Console.WriteLine("Press any key to continue...");
+               Console.ReadKey();
+
+               InitializePlayer();
+
+               Console.Clear();
+          }
+
+          private bool Checking(CheckType type)
+          {
+               while (true)
+               {
+                    if (type == CheckType.Name)
+                         Console.WriteLine($"이름을 {name} 으로 하시겠습니까? Y / N");
+                    else if (type == CheckType.Job)
+                         Console.WriteLine($"직업을 {job} 으로 하시겠습니까? Y / N");
+
+                    string check = Console.ReadLine() ?? "";
+                    check = check.ToLower();
+                    if (check == "y")
+                         return true;
+                    else if (check == "n")
+                         return false;
+                    else continue;
+               }
+          }
+
+          private void Naming()
+          {
+               while (name != null)
+               {
+                    Console.Clear();
+                    Console.WriteLine("이름을 입력해주세요.");
+                    Console.Write("입력: ");
+                    name = Console.ReadLine() ?? "";
+
+                    if (name == null)
+                    {
+                         Console.WriteLine("이름을 다시 입력해주세요.");
+                         continue;
+                    }
+
+                    if (Checking(CheckType.Name))
+                         break;
+               }
+          }
+
+          private void SelectClass()
+          {
+               while (job != null)
+               {
+                    Console.Clear();
+                    Console.WriteLine("직업을 선택하시오.\n1. 전사\n2. 궁수\n3. 도적\n4. 마법사");
+                    Console.Write("입력: ");
+                    string input = Console.ReadLine() ?? "";
+
+                    switch (input)
+                    {
+                         case "1":
+                              job = "전사";
+                              break;
+                         case "2":
+                              job = "궁수";
+                              break;
+                         case "3":
+                              job = "도적";
+                              break;
+                         case "4":
+                              job = "마법사";
+                              break;
+                         default:
+                              Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요.\n");
+                              continue;
+                    }
+
+                    if (Checking(CheckType.Job))
+                         break;
+               }
+          }
+
+          private void InitializePlayer()
+          {
+               player = new Player(name, job);
+
+               Program.Initialize(player);
+          }
+     }
+}
