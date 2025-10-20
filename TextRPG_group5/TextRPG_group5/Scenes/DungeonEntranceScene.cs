3403e9f53f7b3bc311fca388ab69414664b68770
book_dokb <psycholops27@gmail.com>
2025-10-20 17:21:53 +0900

    docs: 일부 텍스트 수정

--- TextRPG_group5/TextRPG_group5/Scenes/DungeonEntranceScene.cs
+++ TextRPG_group5/TextRPG_group5/Scenes/DungeonEntranceScene.cs
@@ -7,59 +7,59 @@ using TextRPG_group5.Managers;
 
 namespace TextRPG_group5.Scenes
 {
-     internal class DungeonEntranceScene : Scene
-     {
-          private Player player;
+    internal class DungeonEntranceScene : Scene
+    {
+        private Player player;
 
-          public DungeonEntranceScene(Player player)
-          {
-               this.player = player;
-          }
+        public DungeonEntranceScene(Player player)
+        {
+            this.player = player;
+        }
 
-          public override void HandleInput(byte input)
-          {
-               if (input == 0)
-               {
-                    // go to MainScene
-                    Program.SetScene(new MainScene(player));
-               }
-               else if (input > player.ReachedStage)
-               {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("잘못된 입력입니다.\n");
-                    Console.ForegroundColor = ConsoleColor.White;
-               }
-               else
-               {
-                    StageManager.Instance.SetPlayer(player);
-                    // go to BattleScene
-                    Battle selectedStage = new Battle(player, StageManager.Instance.CreateMonsters(input), input);
-                    Program.SetScene(new BattleScene(selectedStage));
-                    Console.WriteLine($"{input}층 던전에 입장했습니다.\n");   // log
-               }
-          }
+        public override void HandleInput(byte input)
+        {
+            if (input == 0)
+            {
+                // go to MainScene
+                Program.SetScene(new MainScene(player));
+            }
+            else if (input > player.ReachedStage)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("잘못된 입력입니다.\n");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                StageManager.Instance.SetPlayer(player);
+                // go to BattleScene
+                Battle selectedStage = new Battle(player, StageManager.Instance.CreateMonsters(input), input);
+                Program.SetScene(new BattleScene(selectedStage));
+                Console.WriteLine($"탑의 {input}층 던전에 입장했습니다.\n");   // log
+            }
+        }
 
-          public override void Show()
-          {
-               Console.ForegroundColor = ConsoleColor.Yellow;
-               Console.WriteLine("던전 입구\n");
-               Console.ForegroundColor = ConsoleColor.White;
+        public override void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("소원의 탑 입구\n");
+            Console.ForegroundColor = ConsoleColor.White;
 
-               Console.WriteLine("어둠이 입구를 삼킨다.");
-               //Thread.Sleep(800);
-               Console.WriteLine("이제, 소원을 향한 여정이 다시 시작된다.");
-               //Thread.Sleep(800);
+            Console.WriteLine("소원의 탑이 하늘을 향해 높이 솟아있다.\n");
+            Console.WriteLine("소원을 이루기 위해, 꼭대기로 향하자.\n");
+            //Thread.Sleep(800);
 
-               Console.WriteLine();
-               int stage = player.ReachedStage;
-               stage = stage > Program.maxStage ? Program.maxStage : stage; // 최대 층까지 제한
+            Console.WriteLine();
+            int stage = player.ReachedStage;
+            stage = stage > Program.maxStage ? Program.maxStage : stage; // 최대 층까지 제한
 
-               while (stage > 0)
-               {
-                    Console.WriteLine($"{stage}. {stage}층 던전 입장");
-                    stage--;
-               }
-               Console.WriteLine("0. 마을로 돌아가기");
-          }
-     }
+            while (stage > 0)
+            {
+                Console.WriteLine($"{stage}. 탑의 {stage}층 던전 입장");
+                stage--;
+            }
+            Console.WriteLine();
+            Console.WriteLine("0. 마을로 돌아가기");
+        }
+    }
 }
--- TextRPG_group5/TextRPG_group5/Scenes/DungeonResultScene.cs
+++ TextRPG_group5/TextRPG_group5/Scenes/DungeonResultScene.cs
@@ -8,160 +8,162 @@ using System.Threading.Tasks;
 
 namespace TextRPG_group5.Scenes
 {
-     internal class DungeonResultScene : Scene
-     {
-          private bool isClearStage;
-          private int stage;
-          private Player player;
-          private Player preBattlePlayer;
-          private bool isRewardGet = false;
-          private class Reward
-          {
-               public int Gold;
-               public int Exp;
+    internal class DungeonResultScene : Scene
+    {
+        private bool isClearStage;
+        private int stage;
+        private Player player;
+        private Player preBattlePlayer;
+        private bool isRewardGet = false;
+        private class Reward
+        {
+            public int Gold;
+            public int Exp;
 
-               public Reward(int stage)
-               {
-                    Gold = 200 * stage;
-                    Exp = 15 * stage;
-               }
-          }
+            public Reward(int stage)
+            {
+                Gold = 100 * stage;
+                Exp = 15 * stage;
+            }
+        }
 
-          public DungeonResultScene(Player player, Player preBattlePlayer, int stage, bool isClearStage)
-          {
-               this.player = player;
-               this.preBattlePlayer = preBattlePlayer;
-               this.stage = stage;
-               this.isClearStage = isClearStage;
-          }
+        public DungeonResultScene(Player player, Player preBattlePlayer, int stage, bool isClearStage)
+        {
+            this.player = player;
+            this.preBattlePlayer = preBattlePlayer;
+            this.stage = stage;
+            this.isClearStage = isClearStage;
+        }
 
-          public override void HandleInput(byte input)
-          {
-               if (!isClearStage)
-                    return;
+        public override void HandleInput(byte input)
+        {
+            if (!isClearStage)
+                return;
 
-               switch (input)
-               {
-                    case 0:
-                         if (isClearStage)
-                              Program.SetScene(new DungeonEntranceScene(player));
-                         //else
-                         //     Program.SetScene(new MainScene(player));
-                         break;
-                    default:
-                         Console.ForegroundColor = ConsoleColor.Red;
-                         Console.WriteLine("잘못된 입력입니다.\n");
-                         Console.ForegroundColor = ConsoleColor.White;
-                         break;
-               }
-          }
+            switch (input)
+            {
+                case 0:
+                    if (isClearStage)
+                        Program.SetScene(new DungeonEntranceScene(player));
+                    //else
+                    //     Program.SetScene(new MainScene(player));
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("잘못된 입력입니다.\n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+        }
 
-          public override void Show()
-          {
-               Console.ForegroundColor = ConsoleColor.Yellow;
-               Console.WriteLine("던전 결과\n");
-               Console.ForegroundColor = ConsoleColor.White;
+        public override void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("던전 결과\n");
+            Console.ForegroundColor = ConsoleColor.White;
 
-               // 던전 클리어
-               if (isClearStage)
-               {
-                    Console.WriteLine("===========================================");
-                    Console.WriteLine("           던전 클리어 성공!!!");
-                    Console.WriteLine("===========================================\n");
-                    Thread.Sleep(800);
-                    if (stage == Program.maxStage)
-                    {
-                         Console.WriteLine("마침내, 던전의 끝에 닿았다.\n");
-                         Thread.Sleep(800);
-                         Console.WriteLine("오래된 전설처럼… 네 소원이 이뤄질 시간이 다가온다.\n");
-                         Thread.Sleep(800);
-                         Console.WriteLine("네가 걸어온 길의 끝, 그곳에 ‘진실’이 있었다.\n");
-                         Thread.Sleep(1200);
-                         Console.WriteLine("당신은 여태까지의 모험을 통해서 소원을 스스로 이룰 힘을 얻었다.\n");
-                         Thread.Sleep(800);
-
-                    }
-                    else
-                    {
-                         Console.WriteLine("적은 쓰러지고, 고요가 찾아왔다. 한 걸음 더, 소원에 가까워졌다.\n");
-                         //Thread.Sleep(800);
-                         Console.WriteLine("새로운 길이 열렸다.\n");
-                         //Thread.Sleep(800);
-                         Console.WriteLine("다음 스테이지가 기다린다.\n");
-                         //Thread.Sleep(800);
-                    }
-                    Console.WriteLine();
-               }
-               else
-               {
-                    Console.WriteLine("===========================================");
-                    Console.WriteLine("               던전 클리어 실패...");
-                    Console.WriteLine("===========================================\n");
-                    Thread.Sleep(800);
-                    Console.WriteLine("무릎 꿇은 자리, 숨이 가쁘다.\n");
-                    Thread.Sleep(1200);
-                    Console.WriteLine("이번에는 패배했지만, 끝은 아니다.\n");
-                    Thread.Sleep(800);
-                    Program.SetScene(new MainScene(player));
-               }
-
-               if (!isRewardGet)
-               {
-                    isRewardGet = true;
-                    Result();
-               }
-               if (isClearStage)
-                    Console.WriteLine("0. 돌아가기");
-          }
-
-          private void Result()
-          {
-               if (isClearStage)
-               {
-                    Reward reward = new Reward(stage);
-
-                    player.Gold += reward.Gold; // Player에서 Exp처럼 Gold를 더할 수 있는 메서드 필요
-                    player.GainExp(reward.Exp);
-
+            // 던전 클리어
+            if (isClearStage)
+            {
+                Console.WriteLine("===========================================");
+                Console.WriteLine("           던전 클리어 성공!!!");
+                Console.WriteLine("===========================================\n");
+                Thread.Sleep(800);
+                if (stage == Program.maxStage)
+                {
+                    Console.WriteLine("마침내, 탑 안에 있던 던전을 모두 공략하고 꼭대기 층에 도달했다.\n");
                     Thread.Sleep(1000);
-                    Console.WriteLine("[캐릭터 정보]");
-                    string levelStr = preBattlePlayer.Level.ToString();
-                    string atkStr = preBattlePlayer.Attack.ToString();
-                    string defStr = preBattlePlayer.Defence.ToString();
+                    Console.WriteLine("오래된 전설처럼… 내 소원이 이뤄질 시간이 다가온다.\n");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("탑의 꼭대기, 그곳에 ‘진실이 있었다.\n");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("");
+                    Console.WriteLine("당신은 여태까지의 모험을 통해서 소원을 스스로 이룰 힘을 얻었다.\n");
+                    Thread.Sleep(800);
 
-                    if (preBattlePlayer.Level != player.Level)
-                    {
-                         levelStr = $"{levelStr} -> {player.Level}";
-                         atkStr = $"{atkStr} -> {player.Attack}";
-                         defStr = $"{defStr} -> {player.Defence}";
-                    }
+                }
+                else
+                {
+                    Console.WriteLine("적이 쓰러지고, 문이 열리는 소리가 들린다.\n");
+                    Console.WriteLine("이렇게 한 걸음 더, 소원에 가까워졌다.\n\n");
+                    Console.WriteLine("다음 층으로 향하는 길이 열렸다.\n");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("===========================================");
+                Console.WriteLine("               던전 클리어 실패...");
+                Console.WriteLine("===========================================\n");
+                Thread.Sleep(1000);
+                Console.WriteLine("·\n·\n·\n·\n·");
+                Thread.Sleep(1000);
+                Console.WriteLine("……패배했다.\n");
+                Thread.Sleep(1000);
+                Console.WriteLine("하지만 나는 다시 도전할 거다.\n");
+                Thread.Sleep(1000);
+                Console.WriteLine("……그게 몇 번이 되더라도.\n");
+                Thread.Sleep(1000);
+                Program.SetScene(new MainScene(player));
+            }
 
-                    Console.WriteLine($"이름: {player.Name}"); 
-                    Console.WriteLine($"Lv: {levelStr}");
-                    Console.WriteLine($"HP: {preBattlePlayer.NowHp} -> {player.NowHp}");
-                    Console.WriteLine($"MP: {preBattlePlayer.NowMp} -> {player.NowMp}");
-                    Console.WriteLine($"공격력: {atkStr}");
-                    Console.WriteLine($"방어력: {defStr}");
+            if (!isRewardGet)
+            {
+                isRewardGet = true;
+                Result();
+            }
+            if (isClearStage)
+                Console.WriteLine("0. 돌아가기");
+        }
 
-                    Console.WriteLine("\n[획득 아이템]");
-                    Console.WriteLine($"Gold: {preBattlePlayer.Gold} -> {player.Gold}");
-                    Console.WriteLine($"EXP: {preBattlePlayer.Exp} -> {player.Exp}");
+        private void Result()
+        {
+            if (isClearStage)
+            {
+                Reward reward = new Reward(stage);
 
-                    if (stage == player.ReachedStage) // update player stage
-                    {
-                         Thread.Sleep(1000);
-                         Console.WriteLine("\n최고 층이 갱신되었습니다.");
-                         if (player.ReachedStage < Program.maxStage)
-                              player.ReachedStage++;
-                    }
-                    Console.WriteLine();
-               }
-               else
-               {
-                    player.NowHp = player.MaxHp;
-                    Program.SetSkipInput(true);
-                    Console.Clear();
-               }
-          }
-     }
+                player.Gold += reward.Gold; // Player에서 Exp처럼 Gold를 더할 수 있는 메서드 필요
+                player.GainExp(reward.Exp);
+
+                Thread.Sleep(1000);
+                Console.WriteLine("[캐릭터 정보]");
+                string levelStr = preBattlePlayer.Level.ToString();
+                string atkStr = preBattlePlayer.Attack.ToString();
+                string defStr = preBattlePlayer.Defence.ToString();
+
+                if (preBattlePlayer.Level != player.Level)
+                {
+                    levelStr = $"{levelStr} -> {player.Level}";
+                    atkStr = $"{atkStr} -> {player.Attack}";
+                    defStr = $"{defStr} -> {player.Defence}";
+                }
+
+                Console.WriteLine($"이름: {player.Name}");
+                Console.WriteLine($"Lv: {levelStr}");
+                Console.WriteLine($"HP: {preBattlePlayer.NowHp} -> {player.NowHp}");
+                Console.WriteLine($"MP: {preBattlePlayer.NowMp} -> {player.NowMp}");
+                Console.WriteLine($"공격력: {atkStr}");
+                Console.WriteLine($"방어력: {defStr}");
+
+                Console.WriteLine("\n[획득 아이템]");
+                Console.WriteLine($"Gold: {preBattlePlayer.Gold} -> {player.Gold}");
+                Console.WriteLine($"EXP: {preBattlePlayer.Exp} -> {player.Exp}");
+
+                if (stage == player.ReachedStage) // update player stage
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("\n최고 층이 갱신되었습니다.");
+                    if (player.ReachedStage < Program.maxStage)
+                        player.ReachedStage++;
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                player.NowHp = player.MaxHp;
+                Program.SetSkipInput(true);
+                Console.Clear();
+            }
+        }
+    }
 }
